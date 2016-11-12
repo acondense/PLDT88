3262b270094c875ce62400de28a1fce7b847cf2d
acondense <ariel.conde1997@gmail.com>
2016-11-12 11:48:02 +0800

    PUSH

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import json
 import requests
 from flask import Flask, request
 
-app = Flask(__name__)
+import fbsample # local file will serve as a reference
+import agent
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -17,7 +19,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+    return "PLDT Bot Knows!!!!", 200
 
 
 @app.route('/', methods=['POST'])
@@ -35,11 +37,35 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    if messaging_event['message'].get('is_echo'):
+                        print "An echo returning now"
+                        return "ok", 200
+                    else:
+                        print "Not an echo"
 
-                    send_message(sender_id, "got it, thanks!")
+                    if messaging_event['message'].get('sticker_id'):
+                        print "Thanks for the sticker"
+                        sender_id = messaging_event["sender"]["id"]
+                        fbsample.send_message(sender_id, "Thanks for the sticker")
+                        return "ok", 200
+                    else:
+                        print "Not a sticker"
+
+                    try:
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+
+                        agent.send(sender_id, message_text)
+                    
+                    except:
+                        print "Unable to process"
+                        log(data)
+                        raise
+                    else:
+                        print "Unable to process"
+                        log(data)
+
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -48,34 +74,19 @@ def webhook():
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+                    try:
+                        sender_id = messaging_event["sender"]["id"]
+                        print "POST BACK RECEIVED"
+                        fbsample.send_message(sender_id, "POST BACK RECEIVED")
+                    except:
+                        print "Unable to process"
+                        log(data)
+                        raise
+                    else:
+                        print "Unable to process"
+                        log(data)
 
     return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
