6a310fac947ec90045948a73ae9c60b621da1480
acondense <ariel.conde1997@gmail.com>
2016-11-12 22:50:01 +0800

    PUSH

--- actions.py
+++ actions.py
@@ -53,3 +53,24 @@ def send_recent_bill(recipient_id, data):
 
 
     fbutil.send_file(recipient_id, "http://www.dmv.org/images/bill-of-sale.pdf")
+
+def check_status(recipient_id, data):
+    fbutil.send_bubbles(recipient_id)
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": data['result']['fulfillment']['speech']
+        }
+    })
+
+    # check the user status here assume that he is conneted
+    fbutil.send_message(recipient_id, "All right i checked that your service is active")
+    fbutil.send_bubbles(recipient_id)
+
+    # check if users area is still active
+    fbutil.send_message(recipient_id, "Hi, we are currently doing an infrastructure upgrade in your area")
+    fbutil.send_bubbles(recipient_id)
+    fbutil.send_message(recipient_id, "Expect the connectioin to be back after 8 hrs")
+
