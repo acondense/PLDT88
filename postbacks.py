0301e742e87f657789628128d77181d9e25c33fb
redperiabras <redperiabras@gmail.com>
2016-11-13 04:36:41 +0800

    PUSH

--- postbacks.py
+++ postbacks.py
@@ -90,7 +90,7 @@ def promo_3(recipient_id):
 def trblsht_yes(recipient_id):
     now = datetime.datetime.now()
     strDate = str(now.strftime("%Y%m%d"))
-    output = "Thank you for reporting this. We are making it to the extent of our best to resolve your problem. Here is your file ticket number: " + strDate + ''.join(random.choice(string.ascii_uppercase) for _ in range(6))
+    output = "Thank you for reporting this. We are making it to the extent of our best to resolve your problem. Here is your file ticket number: " + strDate + ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
 
 
     fbutil.send_bubbles(recipient_id)
