cead1aa6bc7a901c83fa2b605763216b4a7eaa41
acondense <ariel.conde1997@gmail.com>
2016-11-12 16:28:16 +0800

    PUSH

--- dispatch.py
+++ dispatch.py
@@ -6,13 +6,14 @@ import actions
 import postbacks
 import fbutil
 
-# refer to actions.py
+# refer to actions.py, plain text from api.ai
 action = {
     'showBestProduct': actions.show_best_product,
     'sendRecentBill': actions.send_recent_bill,
     'input.unknown': actions.uknown
 }
 
+# on button click
 postback = {
     'welcome': postbacks.welcome,
     'welcome_yes': postbacks.welcome_yes,
